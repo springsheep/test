1495d56c4399f16f98b4402b745f7de101ae6f88
z28121 <zhang.pengM@h3c.com>
2024-08-12 09:46:48 +0800

    undefined

--- ceshi/1.sh
+++ ceshi/1.sh
@@ -1,6 +1,9 @@
 var child_process = require("child_process");
+const path = require("path");
 
 const argv = process.argv;
+//把ceshi文件夹下的所有文件提交到远程
+const directory = "/dist";
 
 const msg = argv[2];
 var cmd = `git init  && git add . && git commit -m ${msg} && git remote rm origin && git remote add origin https://github.com/springsheep/test.git && git push -u origin master`;
