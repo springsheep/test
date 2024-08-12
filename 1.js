2784efe82d7aecc681c80d8910542fcf1bf0bb65
z28121 <zhang.pengM@h3c.com>
2024-08-12 09:41:25 +0800

    undefined

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@ var child_process = require("child_process");
 const argv = process.argv;
 
 const msg = argv[2];
-var cmd = `git init  && git add . && git commit -m ${msg} && git remote add origin https://github.com/springsheep/test.git && git push -u origin main`;
+var cmd = `git init  && git add . && git commit -m ${msg} && git remote rm origin && git remote add origin https://github.com/springsheep/test.git && git push -u origin main`;
 child_process.exec(cmd, function (error, stdout, stderr) {
   console.log("error:" + error);
   console.log("stdout:" + stdout);
